d72e46d9b576c619cf8e1bcea49a368be6d21b53
Nicola Iarocci <nicola@nicolaiarocci.com>
2015-01-15 10:09:33 +0100

    Make tests run successfully with Eve 0.5

--- Eve.NET.Tests/Delete.cs
+++ Eve.NET.Tests/Delete.cs
@@ -5,98 +5,98 @@ using System.Net;
 
 namespace Eve.Tests
 {
-    /// <summary>
-    /// Test that DELETE requests to document endpoints are properly executed.
-    /// </summary>
-    [TestFixture]
-    class Delete : MethodsBase
-    {
-         [SetUp]
-        public void DerivedInit()
-        {
-            Init();
+	/// <summary>
+	/// Test that DELETE requests to document endpoints are properly executed.
+	/// </summary>
+	[TestFixture]
+	class Delete : MethodsBase
+	{
+		[SetUp]
+		public void DerivedInit ()
+		{
+			Init ();
 
-            // POST in order to get a valid ETag
-            Original = EveClient.PostAsync<Company>(Endpoint, new Company { Name = "Name" }).Result;
-            Assert.AreEqual(HttpStatusCode.Created, EveClient.HttpResponse.StatusCode);
-        }
+			// POST in order to get a valid ETag
+			Original = EveClient.PostAsync<Company> (Endpoint, new Company { Name = "Name" }).Result;
+			Assert.AreEqual (HttpStatusCode.Created, EveClient.HttpResponse.StatusCode);
+		}
 
-        [Test]
-        public void AcceptEndpointAndObject()
-        {
-            var message = EveClient.DeleteAsync(Endpoint, Original).Result;
-            Assert.AreEqual(HttpStatusCode.OK, message.StatusCode);
+		[Test]
+		public void AcceptEndpointAndObject ()
+		{
+			var message = EveClient.DeleteAsync (Endpoint, Original).Result;
+			Assert.AreEqual (HttpStatusCode.NoContent, message.StatusCode);
 
-            // confirm that item has been deleted on remote
-            message = EveClient.GetAsync(string.Format("{0}/{1}", Endpoint, Original.UniqueId)).Result;
-            Assert.AreEqual(HttpStatusCode.NotFound, message.StatusCode);
-        }
+			// confirm that item has been deleted on remote
+			message = EveClient.GetAsync (string.Format ("{0}/{1}", Endpoint, Original.UniqueId)).Result;
+			Assert.AreEqual (HttpStatusCode.NotFound, message.StatusCode);
+		}
 
-        [Test]
-        public void AcceptObject()
-        {
-            EveClient.ResourceName = Endpoint;
-            var message = EveClient.DeleteAsync(Original).Result;
-            Assert.AreEqual(HttpStatusCode.OK, message.StatusCode);
+		[Test]
+		public void AcceptObject ()
+		{
+			EveClient.ResourceName = Endpoint;
+			var message = EveClient.DeleteAsync (Original).Result;
+			Assert.AreEqual (HttpStatusCode.NoContent, message.StatusCode);
 
-            // confirm that item has been deleted on remote
-            message = EveClient.GetAsync(string.Format("{0}/{1}", Endpoint, Original.UniqueId)).Result;
-            Assert.AreEqual(HttpStatusCode.NotFound, message.StatusCode);
-        }
+			// confirm that item has been deleted on remote
+			message = EveClient.GetAsync (string.Format ("{0}/{1}", Endpoint, Original.UniqueId)).Result;
+			Assert.AreEqual (HttpStatusCode.NotFound, message.StatusCode);
+		}
 
-        [Test]
-        [ExpectedException(typeof(ArgumentNullException), ExpectedMessage="BaseAddress",MatchType= MessageMatch.Contains)]
-        public async Task BaseAddressPropertyNullException()
-        {
-            EveClient.BaseAddress = null;
-            await EveClient.DeleteAsync("resource", Original);
-        }
+		[Test]
+		[ExpectedException (typeof(ArgumentNullException), ExpectedMessage = "BaseAddress", MatchType = MessageMatch.Contains)]
+		public async Task BaseAddressPropertyNullException ()
+		{
+			EveClient.BaseAddress = null;
+			await EveClient.DeleteAsync ("resource", Original);
+		}
 
-        [Test]
-        [ExpectedException(typeof(ArgumentNullException), ExpectedMessage="BaseAddress",MatchType= MessageMatch.Contains)]
-        public async Task BaseAddressPropertyNullExceptionAlt()
-        {
-            EveClient.BaseAddress = null;
-            EveClient.ResourceName = "resource";
-            await EveClient.DeleteAsync(Original);
-        }
+		[Test]
+		[ExpectedException (typeof(ArgumentNullException), ExpectedMessage = "BaseAddress", MatchType = MessageMatch.Contains)]
+		public async Task BaseAddressPropertyNullExceptionAlt ()
+		{
+			EveClient.BaseAddress = null;
+			EveClient.ResourceName = "resource";
+			await EveClient.DeleteAsync (Original);
+		}
 
-        [Test]
-        [ExpectedException(typeof(ArgumentNullException), ExpectedMessage="resourceName",MatchType= MessageMatch.Contains)]
-        public async Task ResourceArgumentNameNullException()
-        {
-            await EveClient.DeleteAsync(null, Original);
-        }
+		[Test]
+		[ExpectedException (typeof(ArgumentNullException), ExpectedMessage = "resourceName", MatchType = MessageMatch.Contains)]
+		public async Task ResourceArgumentNameNullException ()
+		{
+			await EveClient.DeleteAsync (null, Original);
+		}
 
-        [Test]
-        [ExpectedException(typeof(ArgumentException), ExpectedMessage="resourceName",MatchType= MessageMatch.Contains)]
-        public async Task ResourceNameArgumentException()
-        {
-            await EveClient.DeleteAsync(string.Empty, Original);
-        }
+		[Test]
+		[ExpectedException (typeof(ArgumentException), ExpectedMessage = "resourceName", MatchType = MessageMatch.Contains)]
+		public async Task ResourceNameArgumentException ()
+		{
+			await EveClient.DeleteAsync (string.Empty, Original);
+		}
 
-        [Test]
-        [ExpectedException(typeof(ArgumentNullException), ExpectedMessage="obj",MatchType= MessageMatch.Contains)]
-        public async Task ObjArgumentNullException()
-        {
-            await EveClient.DeleteAsync("resource", null);
-        }
+		[Test]
+		[ExpectedException (typeof(ArgumentNullException), ExpectedMessage = "obj", MatchType = MessageMatch.Contains)]
+		public async Task ObjArgumentNullException ()
+		{
+			await EveClient.DeleteAsync ("resource", null);
+		}
 
-        [Test]
-        [ExpectedException(typeof(ArgumentNullException), ExpectedMessage="ResourceName",MatchType= MessageMatch.Contains)]
-        public async Task ResourceNamePropertyNullException()
-        {
-            await EveClient.DeleteAsync(Original);
-        }
+		[Test]
+		[ExpectedException (typeof(ArgumentNullException), ExpectedMessage = "ResourceName", MatchType = MessageMatch.Contains)]
+		public async Task ResourceNamePropertyNullException ()
+		{
+			await EveClient.DeleteAsync (Original);
+		}
 
 
-        [Test]
-        [ExpectedException(typeof(ArgumentNullException), ExpectedMessage="obj",MatchType= MessageMatch.Contains)]
-        public async Task ObjArgumentNullExceptionAlt()
-        {
-            EveClient.ResourceName = "resource";
-            await EveClient.DeleteAsync(null);
-        }
+		[Test]
+		[ExpectedException (typeof(ArgumentNullException), ExpectedMessage = "obj", MatchType = MessageMatch.Contains)]
+		public async Task ObjArgumentNullExceptionAlt ()
+		{
+			EveClient.ResourceName = "resource";
+			await EveClient.DeleteAsync (null);
+		}
 
-    }
+	}
 }
--- Eve.NET.Tests/MethodsBase.cs
+++ Eve.NET.Tests/MethodsBase.cs
@@ -6,21 +6,22 @@ using NUnit.Framework;
 
 namespace Eve.Tests
 {
-    public class MethodsBase
-    {
-        // In order to run the test suite you should clone the evenet-testbed repo:
-        // https://github.com/nicolaiarocci/Eve.NET-testbed
+	public class MethodsBase
+	{
+		// In order to run the test suite you should clone the evenet-testbed repo:
+		// https://github.com/nicolaiarocci/Eve.NET-testbed
 
-        // Run the webservice and update the Service const below accordingly. Alternatively 
-        // you can run the tests againsts a free test instance which is running on the web 
-        // (see below).
+		// Run the webservice and update the Service const below accordingly. Alternatively
+		// you can run the tests againsts a free test instance which is running on the web
+		// (see below).
 
-#if (VM)
+		#if (VM)
         // VIRTUALBOX IMAGE
         // In my case I am running Windows in a VirtualBox VM so in order to access the 
         // OSX Host 'localhost' where a local instance of the REST API is running, I use 
         // the standard 10.0.2.2:5000 uri.
         internal const string Service = "http://10.0.2.2:5000/";
+
 #elif (TESTBED)
         // REMOTE INSTANCE
         // If you don't have a local Eve.NET-testbed instance then you can opt to run the 
@@ -28,68 +29,69 @@ namespace Eve.Tests
         // slower though and please, don't overuse it: the webservice runs on very limited 
         // resources and we want everyone to keep enjoying it.
         internal const string Service = "http://evenet-testbed.herokuapp.com";
+
 #else
-        // LOCAL INSTANCE
-        internal const string Service = "http://localhost:5000/";
-#endif
+		// LOCAL INSTANCE
+		internal const string Service = "http://localhost:5000/";
+		#endif
 
-        internal const string Endpoint = "companies";
-        internal EveClient EveClient;
-        internal Company Original;
+		internal const string Endpoint = "companies";
+		internal EveClient EveClient;
+		internal Company Original;
 
-        [SetUp]
-        public void Init()
-        {
-            // Make sure remote remote endpoint is completely empty.
-            // We use the standard HttpClient for this (we aren't testing anything yet).
-            var hc = new HttpClient { BaseAddress = new Uri(Service) };
-            Assert.IsTrue(hc.DeleteAsync(Endpoint).Result.StatusCode == HttpStatusCode.OK);
+		[SetUp]
+		public void Init ()
+		{
+			// Make sure remote remote endpoint is completely empty.
+			// We use the standard HttpClient for this (we aren't testing anything yet).
+			var hc = new HttpClient { BaseAddress = new Uri (Service) };
+			Assert.IsTrue (hc.DeleteAsync (Endpoint).Result.StatusCode == HttpStatusCode.NoContent);
 
-            // Ok let's roll now.
-            EveClient = new EveClient(Service);
-        }
+			// Ok let's roll now.
+			EveClient = new EveClient (Service);
+		}
 
-        /// <summary>
-        /// Validate that two Company instances are equal (properties have same values)
-        /// </summary>
-        /// <param name="original">First instance.</param>
-        /// <param name="result">Second instance.</param>
-        public void ValidateAreEquals(Company original, Company result)
-        {
-            Assert.AreEqual(original.UniqueId, result.UniqueId);
-            Assert.AreEqual(original.ETag, result.ETag);
-            Assert.AreEqual(original.Created, result.Created);
-            Assert.AreEqual(original.Updated, result.Updated);
-            Assert.AreEqual(original.Name, result.Name);
-            Assert.AreEqual(original.Password, result.Password);
-        }
+		/// <summary>
+		/// Validate that two Company instances are equal (properties have same values)
+		/// </summary>
+		/// <param name="original">First instance.</param>
+		/// <param name="result">Second instance.</param>
+		public void ValidateAreEquals (Company original, Company result)
+		{
+			Assert.AreEqual (original.UniqueId, result.UniqueId);
+			Assert.AreEqual (original.ETag, result.ETag);
+			Assert.AreEqual (original.Created, result.Created);
+			Assert.AreEqual (original.Updated, result.Updated);
+			Assert.AreEqual (original.Name, result.Name);
+			Assert.AreEqual (original.Password, result.Password);
+		}
 
-        /// <summary>
-        /// Validate that the HttpResponseMessage can be casted to a Company and that its values match the original instance.
-        /// </summary>
-        /// <param name="responseMessage">HttoResponseMessage.</param>
-        /// <param name="original">Original Company instance.</param>
-        public void ValidateReturnedHttpResponse(HttpResponseMessage responseMessage, Company original)
-        {
-            var s = responseMessage.Content.ReadAsStringAsync ().Result;
-            var c = JsonConvert.DeserializeObject<Company> (s);
-            ValidateReturnedObject(c, original);
-        }
+		/// <summary>
+		/// Validate that the HttpResponseMessage can be casted to a Company and that its values match the original instance.
+		/// </summary>
+		/// <param name="responseMessage">HttoResponseMessage.</param>
+		/// <param name="original">Original Company instance.</param>
+		public void ValidateReturnedHttpResponse (HttpResponseMessage responseMessage, Company original)
+		{
+			var s = responseMessage.Content.ReadAsStringAsync ().Result;
+			var c = JsonConvert.DeserializeObject<Company> (s);
+			ValidateReturnedObject (c, original);
+		}
 
-        /// <summary>
-        /// Validate that a Company instance is valid (has all remote service meta field values) and similar to original instance.
-        /// </summary>
-        /// <param name="obj">Company instance to be tested.</param>
-        /// <param name="original">Similar instance.</param>
-        /// <remarks>Since we are only changing Name property values, we only make sure that Name values match.</remarks>
-        public void ValidateReturnedObject(Company obj, Company original)
-        {
-            Assert.IsNotNull(obj);
-            Assert.IsInstanceOf<DateTime>(obj.Created);
-            Assert.IsInstanceOf<DateTime>(obj.Updated);
-            Assert.IsNotNullOrEmpty(obj.UniqueId);
-            Assert.IsNotNullOrEmpty(obj.ETag);
-            Assert.AreEqual(obj.Name, original.Name);
-        }
-    }
+		/// <summary>
+		/// Validate that a Company instance is valid (has all remote service meta field values) and similar to original instance.
+		/// </summary>
+		/// <param name="obj">Company instance to be tested.</param>
+		/// <param name="original">Similar instance.</param>
+		/// <remarks>Since we are only changing Name property values, we only make sure that Name values match.</remarks>
+		public void ValidateReturnedObject (Company obj, Company original)
+		{
+			Assert.IsNotNull (obj);
+			Assert.IsInstanceOf<DateTime> (obj.Created);
+			Assert.IsInstanceOf<DateTime> (obj.Updated);
+			Assert.IsNotNullOrEmpty (obj.UniqueId);
+			Assert.IsNotNullOrEmpty (obj.ETag);
+			Assert.AreEqual (obj.Name, original.Name);
+		}
+	}
 }
