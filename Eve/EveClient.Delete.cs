2fe9a69c11d27f06c68ac1d8cbd9f7524ad2a47c
Pedro Rodrigues <prodrigues1990@gmail.com>
2018-03-07 06:46:48 +0000

    removed region blocks, moved to partial class
    its a bit more readable

---
+++ Eve/EveClient.Delete.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Eve
+{
+    public partial class EveClient : IDisposable
+    {
+        /// <summary>
+        /// Performs an asynchronous DELETE request on a document endpoint.
+        /// </summary>
+        /// <returns>The raw response returned by the service.</returns>
+        /// <param name="resourceName">Resource name.</param>
+        /// <param name="obj">Object to be deleted on the service.</param>
+        public async Task<HttpResponseMessage> DeleteAsync(string resourceName, object obj)
+        {
+
+            ValidateBaseAddress();
+            if (resourceName == null)
+            {
+                throw new ArgumentNullException("resourceName");
+            }
+            if (resourceName == string.Empty)
+            {
+                throw new ArgumentException("resourceName");
+            }
+            if (obj == null)
+            {
+                throw new ArgumentNullException("obj");
+            }
+
+            using (var client = new HttpClient())
+            {
+                SettingsForEditing(client, obj);
+                HttpResponse = await client.DeleteAsync(string.Format("{0}/{1}", resourceName, GetDocumentId(obj))).ConfigureAwait(false);
+                return HttpResponse;
+            }
+        }
+
+        /// <summary>
+        /// Performs an asynchronous DELETE request on a document endpoint
+        /// </summary>
+        /// <returns>The raw response returned by the service.</returns>
+        /// <param name="obj">Object to be deleted from the service.</param>
+        public async Task<HttpResponseMessage> DeleteAsync(object obj)
+        {
+            ValidateResourceName();
+            HttpResponse = await DeleteAsync(ResourceName, obj);
+            return HttpResponse;
+        }
+
+        /// <summary>
+        /// Performs an asynchronous DELETE request on a resource endpoint
+        /// </summary>
+        /// <returns>The raw response returned by the service.</returns>
+        /// <remarks>Use with caution as this is likely to delete all endpoint data.</remarks>
+        public async Task<HttpResponseMessage> DeleteAsync()
+        {
+            ValidateBaseAddress();
+            ValidateResourceName();
+
+            using (var client = new HttpClient())
+            {
+                Settings(client);
+                HttpResponse = await client.DeleteAsync(ResourceName).ConfigureAwait(false);
+                return HttpResponse;
+            }
+        }
+
+        /// <summary>
+        /// Performs an asynchronous DELETE request on a resource endpoint
+        /// </summary>
+        /// <returns>The raw response returned by the service.</returns>
+        /// <param name="resourceName">Resource endpoint.</param>
+        /// <remarks>Use with caution as this is likely to delete all endpoint data.</remarks>
+        public async Task<HttpResponseMessage> DeleteAsync(string resourceName)
+        {
+            ValidateBaseAddress();
+            if (resourceName == null)
+            {
+                throw new ArgumentNullException("resourceName");
+            }
+            if (resourceName == string.Empty)
+            {
+                throw new ArgumentException("resourceName");
+            }
+
+            using (var client = new HttpClient())
+            {
+                Settings(client);
+                HttpResponse = await client.DeleteAsync(resourceName).ConfigureAwait(false);
+                return HttpResponse;
+            }
+        }
+    }
+}
---
+++ Eve/EveClient.Properties.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Eve.Authenticators;
+using System.Reflection;
+using Newtonsoft.Json.Serialization;
+
+namespace Eve
+{
+    public partial class EveClient : IDisposable
+    {
+        /// <summary>
+        /// Gets or sets the remote service base address.
+        /// </summary>
+        /// <value>The remote service base address.</value>
+        public Uri BaseAddress { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the resource endpoint.
+        /// </summary>
+        /// <value>The name of the resource endpoint.</value>
+        public string ResourceName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the document identifier.
+        /// </summary>
+        /// <value>The document identifier.</value>
+        /// <remarks>Used in conjuction with BaseAddress and ResourceName to construct the document endpoint.</remarks>
+        public string DocumentId { get; set; }
+
+        /// <summary>
+        /// Represents a HTTP response message.
+        /// </summary>
+        /// <value>The http response.</value>
+        public HttpResponseMessage HttpResponse { get; private set; }
+
+        /// <summary>
+        /// Gets or sets the authenticator.
+        /// </summary>
+        /// <value>The authenticator.</value>
+        public IAuthenticator Authenticator { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the LastUpdated field.
+        /// </summary>
+        public string LastUpdatedField { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the Deleted field.
+        /// </summary>
+        public string DeletedField { get; set; }
+        /// <summary>
+        /// Gets or set the custom headers to be included with the request.
+        /// </summary>
+        public Dictionary<string, string> CustomHeaders { get; set; }
+    }
+}
