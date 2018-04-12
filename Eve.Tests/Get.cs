a2835cb4e07e629a804c0647f65acd2a32d0a26b
Nicola Iarocci <nicola@nicolaiarocci.com>
2018-04-12 15:08:53 +0200

    first experiments

---
+++ Eve.Tests/Get.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Eve.EC;
+
+namespace Eve.Tests
+{
+    [TestFixture]
+    class Fluent
+    {
+        [Test]
+        public void IfModifiedSince()
+        {
+            var r = Get<Company>().Where(x => x.Name == "ciao" && x.ETag == "tag");
+        }
+    }
+}
---
+++ Eve/Get.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+
+namespace Eve
+{
+    public static class EC
+    {
+        public static Getter<T> Get<T>()
+        {
+            return new Getter<T>();
+        }
+        public class Getter<T>
+        {
+            public Getter() { }
+            public List<T> Where(Expression<Func<T, bool>> predicate)
+            {
+                var param = predicate.Parameters[0];
+                var operation = (BinaryExpression)predicate.Body;
+                var left = operation.Left;
+                var right = operation.Right;
+                return new List<T>();
+
+            }
+        }
+    }
+}
